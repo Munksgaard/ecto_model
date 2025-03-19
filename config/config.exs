8be1d3ab9c20b0f7e147543969c773b6e48c340a
Philip Munksgaard <philip@munksgaard.me>
2025-03-19 13:35:57 +0100

    Move reading of environment variables to runtime.exs

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,5 @@ import Config
 config :ecto_model, ecto_repos: [EctoModel.Repo], table_schema: "public"
 
 config :ecto_model, EctoModel.Repo,
-  database: System.fetch_env!("POSTGRES_DB"),
-  username: System.fetch_env!("POSTGRES_USER"),
-  password: System.fetch_env!("POSTGRES_PASSWORD"),
   pool: Ecto.Adapters.SQL.Sandbox,
   hostname: "localhost"
---
+++ config/runtime.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :ecto_model, EctoModel.Repo,
+  database: System.fetch_env!("POSTGRES_DB"),
+  username: System.fetch_env!("POSTGRES_USER"),
+  password: System.fetch_env!("POSTGRES_PASSWORD")
